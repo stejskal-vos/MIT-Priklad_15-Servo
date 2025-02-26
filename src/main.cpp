fd2f2b22e96cfcbe9c71204c8833567a522ec90a
Pavel Stejskal <stejskal@voskh.cz>
2025-02-26 21:50:30 +0100

    daaalsi commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+
+
+void setup() {
+pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13,1);
+  delay(500);
+  digitalWrite(13,0);
+  delay(500);
+  digitalWrite(13,1);
+  delay(500);
+  digitalWrite(13,0);
+  delay(500);
+  // nic
+  }
+
+
