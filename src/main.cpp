1b4c1ef916a7e653fe540e83726cdac44da3702d
Pavel Stejskal <stejskal@voskh.cz>
2025-02-26 22:09:31 +0100

    3. commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ pinMode(13, OUTPUT);
 
 void loop() {
   digitalWrite(13,1);
-  delay(500);
+  delay(200);
   digitalWrite(13,0);
-  delay(500);
+  delay(200);
   digitalWrite(13,1);
   delay(500);
   digitalWrite(13,0);
